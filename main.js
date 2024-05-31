e968fb72c6b4ee1101b259c7fcf3ebec963ff54d
Mbali-Nyalungu <Mbali.Nyalungu04@gmail.com>
2024-05-31 15:21:07 +0200

    pearls

---
+++ main.js
@@ -0,0 +1,54 @@
+// let =num1;
+//  i =num2;
+//  operator= operator;
+
+
+
+
+
+num1 = prompt ('enter your first number')
+
+num2 = prompt ('enter your second number')
+
+
+op = prompt ('enter your operator')
+
+
+if (op ==='+') {
+
+
+    console.log ('your answer is' + (num1 +  num2 ))
+    
+
+}
+
+else if ( op==='-') {
+    console.log ('Answer is ' + (num1 - num2))
+
+} else if (op === "*");
+
+    else if (op ==='/')
+
+        {
+            console.log(num1/num2)
+        }
+        
+        else
+        
+      {
+        console.log('invalid operator')
+  
+
+
+    }
+
+
+// op = prompt ('enter your 1st number');
+// console.log(num1);
+
+// op = prompt ( 'enter your second number');
+// console.log(num2);
+
+
+// op = prompt ('your answer is')
+// console.log( 'your answer is'  + ( num1 + num2));
